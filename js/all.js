00e33f46827c69480f87b656615030bd91e4ace9
unayo <anewsdada2013@gmail.com>
2020-05-28 00:30:48 +0800

    hamburger js

--- js/all.js
+++ js/all.js
@@ -1,9 +1,30 @@
-$(document).ready(function() {
-    // menu hide
-    $('.humburger-open').click(function(event) {
-        // Act on the event
-        $('.menu-m').slideToggl();
+// $(document).ready(function() {
+//     $('.ham-icon').click(function(event) {
+//         $('.dropdown').slideToggle();
+//     });
+// });
 
-    });
-
-});
+$('.ham-icon').click(function(e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active')
+  })
+  
+  
+  // 隱藏所有的 li 內文
+  $('.card-item-body').hide();
+  // 預設讓第一個 li 加上 active
+  $('.card-item').eq(0).addClass('active');
+  // 預設讓第一個 li 內文 顯示
+  $('.card-item-body').eq(0).show();
+  
+  // 點擊 li
+  $('.card-item').click(function (e) {
+    // 取消預設 event 事件
+    e.preventDefault(); 
+    // 加上 active 屬性，並把其他有 active 的移除
+    $(this).toggleClass('active').siblings().removeClass('active');
+    // 讓自己的 li body 切換收合
+    $(this).children().next().slideToggle();
+    // 讓其他 li body 收合
+    $(this).siblings().children().next().slideUp();
+  })
